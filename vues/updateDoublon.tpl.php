4bb4ff6e0f92e8924b4c7c42c3f6bbf0035c2a7c
drdam <drdam@cassini>
2013-07-04 20:45:10 +0200

    Correction orthographe

--- vues/updateDoublon.tpl.php
+++ vues/updateDoublon.tpl.php
@@ -39,10 +39,10 @@
 
 L'indice de coformité est un rapport de comparaison de la forme x-y-z
 <ul><li>
-    Le premier chiffre correspond au nombre d'élements testé dans la fact.
+    Le premier chiffre correspond au nombre d'éléments testés dans la fact.
     </li><li> 
-     Le Second chiffre correspond au nombre d'élements testé dans la référence.
+     Le second chiffre correspond au nombre d'éléments testés dans la référence.
     </li><li>
-     Le troisieme chiffre correspond au nombre de d'élements commun.
+     Le troisieme chiffre correspond au nombre de d'éléments en commun.
 </li></ul>
-les mot "chuck" et "norris" ne sont pas testé comme les mot de moins de 2 caractères
+les mot "chuck" et "norris" ne sont pas testés comme les mots de moins de 2 caractères
